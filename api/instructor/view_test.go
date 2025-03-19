d5c08f220164c35cb717180e9338be79652627fe
Junfeng Rong <rongjunfeng09@gmail.com>
2025-03-19 12:55:53 -0400

    add unit tests

--- api/instructor/view_test.go
+++ api/instructor/view_test.go
@@ -1,9 +1,10 @@
 package instructor
 
 import (
+	"dance/models"
+	"dance/types"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,14 +16,123 @@ func SetUpRouter() *gin.Engine {
 }
 
 func TestDeleteInstructorHandle(t *testing.T) {
-	mockResponse := `{"code":0,"data":null,"msg":"ok"}`
+	var resp types.BaseResponse
 	r := SetUpRouter()
-	r.DELETE("/:id", DeleteInstructorHandle)
-	req, _ := http.NewRequest("DELETE", "/11", nil)
+	r.DELETE("/instructors/:id", DeleteInstructorHandle)
+	req, _ := http.NewRequest("DELETE", "/instructors/11", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if err := types.BuildResponse(w.Body, &resp); err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+	if err := resp.Verify(); err != nil {
+		assert.Fail(t, err.Error())
+	}
+}
+
+type InstructorListResponse struct {
+	types.BaseResponse
+	Data []interface{} `json:"data"`
+}
+
+func TestGetInstructorListHandle(t *testing.T) {
+	var resp InstructorListResponse
+	r := SetUpRouter()
+	r.GET("/instructors", GetInstructorListHandle)
+	req, _ := http.NewRequest("GET", "/instructors", nil)
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 
-	responseData, _ := io.ReadAll(w.Body)
-	assert.Equal(t, mockResponse, string(responseData))
-	assert.Equal(t, http.StatusOK, w.Code)
+	if err := types.BuildResponse(w.Body, &resp); err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+	if err := resp.Verify(); err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+	if len(resp.Data) == 0 {
+		assert.Fail(t, "No record in Instructor list.")
+	}
+
+	//types.PrintJson(resp.Data)
+}
+
+type GetInstructorResponse struct {
+	types.BaseResponse
+	Data models.Instructor `json:"data"`
+}
+
+func TestGetInstructorHandle(t *testing.T) {
+	var resp GetInstructorResponse
+	r := SetUpRouter()
+	r.GET("/instructors/:id", GetInstructorHandle)
+	req, _ := http.NewRequest("GET", "/instructors/1", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	if err := types.BuildResponse(w.Body, &resp); err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+	if err := resp.Verify(); err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+	if resp.Data.ID != 1 {
+		assert.Fail(t, "Fail to get Instructor")
+	}
+
+}
+
+func TestUpdateInstructorHandle(t *testing.T) {
+	var resp types.BaseResponse
+	form := types.UpdateInstructorRequest{
+		Name: types.GetStringPtr("test123"),
+	}
+	br, _ := types.StructToReader(form)
+	r := SetUpRouter()
+	r.PUT("/instructors/:id", UpdateInstructorHandle)
+
+	req, _ := http.NewRequest("PUT", "/instructors/1", br)
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	if err := types.BuildResponse(w.Body, &resp); err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+	if err := resp.Verify(); err != nil {
+		assert.Fail(t, err.Error())
+	}
+}
+
+func TestCreateInstructorHandle(t *testing.T) {
+	var resp types.BaseResponse
+	form := types.CreateInstructorRequest{
+		models.Instructor{
+			ID:   4,
+			Name: "test123",
+		},
+	}
+	br, _ := types.StructToReader(form)
+	r := SetUpRouter()
+	r.POST("/instructors", CreateInstructorHandle)
+
+	req, _ := http.NewRequest("POST", "/instructors", br)
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	if err := types.BuildResponse(w.Body, &resp); err != nil {
+		assert.Fail(t, err.Error())
+		return
+	}
+	if err := resp.Verify(); err != nil {
+		assert.Fail(t, err.Error())
+	}
 }
--- types/common.go
+++ types/common.go
@@ -1,5 +1,14 @@
 package types
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
 type PaginatorRequest struct {
 	Page     int64 `json:"page" form:"page" default:"1"`
 	PageSize int64 `json:"pageSize" form:"pageSize" default:"10"`
@@ -14,4 +23,49 @@ func GetStringVal(s *string) string {
 		return *s
 	}
 	return ""
+}
+
+type BaseResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+func (r BaseResponse) Verify() error {
+	if r.Code == 0 {
+		return nil
+	}
+	return errors.New(r.Msg)
+}
+
+func BuildResponse(r io.Reader, v interface{}) error {
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(body, v)
+}
+
+func ToJsonString(data interface{}) string {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Sprintf("to json err: %s", err.Error())
+	}
+	return string(b)
+}
+
+func PrintJson(data interface{}) {
+	b, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		fmt.Println(data)
+		return
+	}
+	fmt.Println(string(b))
+}
+
+func StructToReader(p interface{}) (io.Reader, error) {
+	data, err := json.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewBuffer(data), nil
 }
